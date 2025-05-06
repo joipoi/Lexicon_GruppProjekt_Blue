dcf52bec1650607bf63272d42e69b54b290bd433
joipoi <joel@ekne.com>
2025-05-06 14:55:10 +0200

    Created RecipeSearch component with filtering logic

--- src/pages/Planning.jsx
+++ src/pages/Planning.jsx
@@ -1,8 +1,12 @@
+import RecipeSearch from '../componenets/RecipeSearch'
+
 function Planning() {
     return (
       <div>
         <h1>Planning</h1>
         <p>This is the Planning page content.</p>
+        <h1 className="text-2xl font-bold mb-4">Recipe Finder</h1>
+        <RecipeSearch />
       </div>
     );
   }
