879438d4e8e8c8f46a1f84c1f83e1826b7ae09f3
Kalle <kallebjorfors@gmail.com>
2025-05-13 14:15:14 +0200

    Update asset imports for consistency and clarity

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,3 @@
-import header_bg_color from './header-bg-color.png';
 import fijoka_logo_sky from './fijoka_logo_sky.png';
 import fijoka_logo_terracota from './fijoka_logo_terracota.png';
 import fijoka_logo_vit from './fijoka_logo_vit.png';
@@ -13,7 +12,6 @@ import LogoVit from './LogoVit.png';
 import LogoUmber from './LogoUmber.png';
 
 export const assets = {
-  header_bg_color,
   fijoka_logo_sky,
   fijoka_logo_terracota,
   fijoka_logo_vit,
